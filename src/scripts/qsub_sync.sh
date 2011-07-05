fc0c30ff3fa8d68d630f3e0fa4815e0031afa08a
Joachim Gabler <JGabler@univa.com>
2011-07-05 10:19:00 +0200

    Enhancements to testsuite test functional/event_client/max_dyn_ec

--- src/scripts/qsub_sync.sh
+++ src/scripts/qsub_sync.sh
@@ -15,6 +15,9 @@ x=0
 while [ $x -lt $amount ]; do
    qsub -sync y -b y -o /dev/null -j y sleep $duration &
    x=`expr $x + 1`
+   if [ `expr $x % 5` -eq 0 ]; then
+      sleep 1
+   fi
 done
 
 # wait for the qsub -sync to exit
