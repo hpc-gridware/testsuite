46f35cca828d52f589f52d69dc35a196d1c26eae
Joachim Gabler <jgabler@hpc-gridware.com>
2023-12-11 13:19:10 +0100

    EH: OGE-22 make testsuite run on current OSes in our tests environments // add new commandline option vncserver <hostname>:<display> creating a vnc session on the given display

---
+++ src/tcl_files/display.tcl
@@ -0,0 +1,72 @@
+proc check_display {} {
+     global CHECK_DISPLAY_OUTPUT CHECK_USER
+
+     if { [ string compare $CHECK_DISPLAY_OUTPUT "undefined" ] == 0 } {
+        puts "no debug x display set"
+        return -1
+     }
+
+     if { [ have_root_passwd ] != 0 } {
+         set_root_passwd
+     }
+
+     ts_log_fine "using display: $CHECK_DISPLAY_OUTPUT"
+
+     set local_host [gethostname]
+     set xterm_path [get_binary_path $local_host "xterm"]
+
+     start_remote_prog $local_host "$CHECK_USER" $xterm_path "-bg darkolivegreen -fg navajowhite -sl 5000 -sb -j -display $CHECK_DISPLAY_OUTPUT -e sleep 1"
+     if { $prg_exit_state != 0 } {
+         puts "can't open display $CHECK_DISPLAY_OUTPUT as user $CHECK_USER from host $local_host"
+         return -1
+     }
+
+     if { [ have_root_passwd ] != 0 } {
+         set_root_passwd
+     }
+     start_remote_prog "$local_host" "root" $xterm_path "-bg darkolivegreen -fg navajowhite -sl 5000 -sb -j -display $CHECK_DISPLAY_OUTPUT -e sleep 1"
+     if { $prg_exit_state != 0 } {
+         puts "can't open display $CHECK_DISPLAY_OUTPUT as user root from host $local_host"
+         return -1
+     }
+     return 0
+}
+
+proc start_vncserver {host display} {
+   global CHECK_USER
+
+   set cmd "nohup"
+   set args "vncserver :$display"
+   set output [start_remote_prog $host $CHECK_USER $cmd $args dummy 60 1]
+   ts_log_fine $output
+}
+
+proc check_start_vncserver {} {
+   global CHECK_USER
+   global CHECK_DISPLAY_OUTPUT
+
+   if {[string first ":" $CHECK_DISPLAY_OUTPUT] <= 0} {
+      puts "vncserver display must be given in the form <hostname>:<display>"
+      testsuite_shutdown 1
+   }
+   set vnc_host [lindex [split $CHECK_DISPLAY_OUTPUT ":"] 0]
+   set vnc_display [lindex [split $CHECK_DISPLAY_OUTPUT ":"] 1]
+   if {$vnc_host == "" || $vnc_display == ""} {
+      puts "vncserver display must be given in the form <hostname>:<display>"
+      testsuite_shutdown 1
+   }
+
+   ts_log_fine "starting if there is a X-server running on $vnc_host display $vnc_display"
+   if {[check_display] == 0} {
+      ts_log_fine "vnc server / X-server $vnc_display on host $vnc_host is already running"
+   } else {
+      ts_log_fine "starting VNC server $vnc_display on host $vnc_host"
+      start_vncserver $vnc_host $vnc_display
+      if {[check_display] != 0} {
+         ts_log_fine "VNC server $vnc_display on host $vnc_host does not work"
+         testsuite_shutdown 1
+      }
+   }
+}
+
+
