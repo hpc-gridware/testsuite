bfd5f0aea00031f6bab7dfcb1d13a594b1f546f8
Ernst Bablick <ebablick@hpc-gridware.com>
2024-07-10 11:37:42 +0000

    BF: CS-403: qmaster size fails

--- src/jenkins_generate_day.groovy
+++ src/jenkins_generate_day.groovy
@@ -2,3 +2,17 @@ folder('GCS-CI-CD/TestsDay') {
    displayName('TestsDay')
    description('Tests in level day of GCS-CI-CD environemnt')
 }
+job('GCS-CI-CD/TestsDay/throughput') {
+   lockableResources {
+      label('gcs-ci-cd-cluster')
+      resourcesVariable('CLUSTER')
+      resourceNumber(1)
+   }
+   wrappers {
+      sshAgent('tstusr-rsa-key')
+   }
+   steps {
+      shell('ssh tstusr@h007 /tools/CS/bin/gcs-ci-cd ${CLUSTER} check /home/tstusr/CS/gcs-ci-cd-0/testsuite/src/checktree/performance/throughput 300')
+   }
+}
+queue('GCS-CI-CD/TestsDay/throughput')
