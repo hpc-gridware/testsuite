2f3f24a3590245b0dd241ca12ad4e11ece1290fc
Joachim Gabler <jgabler@hpc-gridware.com>
2024-09-30 11:10:19 +0200

    EH: CS-629 testsuite: enhance inefficiant deletion of local tmp files

--- src/scripts/delete_temp_files.sh
+++ src/scripts/delete_temp_files.sh
@@ -3,8 +3,10 @@
 # script to be called on the file server to delete all the temp files
 # created by testsuite
 # first argument is the file listing the temp filenames
-if [ $# -ne 1 ]; then
-   echo "Usage: $0 filename"
+#echo "$0 running on host `hostname`"
+
+if [ $# -lt 1 ]; then
+   echo "Usage: $0 filename [hostname]"
    exit 1
 fi
 
@@ -14,12 +16,30 @@ if [ ! -f $orig ]; then
    exit 0
 fi
 
+hostname="NONE"
+if [ $# -eq 2 ]; then
+   hostname=$2
+fi
+
 # delete the files
-for file in `cat $orig`; do
+for line in `cat $orig`; do
+   if [ "$host" = "NONE" ]; then
+      file=$line
+   else
+      host=`echo $line | cut -f 1 -d :`
+      if [ "$host" != "$hostname" ]; then
+         continue
+      fi
+      file=`echo $line | cut -f 2 -d :`
+   fi
+
+#   echo "file is $file"
    if [ -f "$file" ]; then
+#      echo "deleting file $file"
       rm -f $file
    else
       if [ -d "$file" ]; then
+#         echo "deleting directory $file"
          rm -rf $file
       fi
    fi
