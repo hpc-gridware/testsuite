e079b4e5cf982fc2a603da7cadf5de2d9e72af69
Joachim Gabler <jgabler@hpc-gridware.com>
2024-10-08 12:54:31 +0200

    improved max_dyn_ec test // BelongsTo: CS-666

--- src/scripts/qsub_sync.sh
+++ src/scripts/qsub_sync.sh
@@ -7,17 +7,37 @@
 amount=$1
 duration=$2
 
+start_time=0
+if [ $# -ge 3 ]; then
+   start_time=$3
+fi
+
 # do not inherit SGE_DEBUG_LEVEL
 unset SGE_DEBUG_LEVEL
+
+ARCH=`$SGE_ROOT/util/arch`
+
+# wait until start_time
+# we want to submit all jobs (from all hosts where we started this script) at the same time
+# to put even more load on sge_qmaster / event master
+if [ $start_time -gt 0 ]; then
+   NOW="$SGE_ROOT/utilbin/$ARCH/now"
+   while [ `$NOW` -lt $start_time ]; do
+      sleep 1
+   done
+fi
+
+# workaround for CS-666
+# when we set the event client interval to 15 seconds (instead of the default 30 seconds)
+# the issue does not happen
+SGE_JAPI_EDTIME=15
+export SGE_JAPI_EDTIME
 
 # submit n qsub -sync sleeper
 x=0
 while [ $x -lt $amount ]; do
    qsub -sync y -b y -o /dev/null -j y sleep $duration &
    x=`expr $x + 1`
-   if [ `expr $x % 5` -eq 0 ]; then
-      sleep 1
-   fi
 done
 
 # wait for the qsub -sync to exit
