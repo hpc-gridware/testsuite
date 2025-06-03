d87095171172a573e235019f77d64c1670def193
Ernst Bablick <ebablick@hpc-gridware.com>
2025-06-03 21:18:19 +0200

    TA: CS-1299: Evaluate lcov and do an integration with our test environment (added comments)

--- src/tcl_files/coverage_lcov.tcl
+++ src/tcl_files/coverage_lcov.tcl
@@ -45,6 +45,13 @@ proc lcov_get_compile_hosts {} {
    return [compile_unify_host_list $compile_hosts]
 }
 
+## @brief returns the directory where the lcov coverage data is stored
+#
+# The directory is based on the configured test suite commd_port and the host name.
+#
+# @param host the host name for which the coverage data directory is returned
+# @return the directory where the lcov coverage data is stored
+#
 proc lcov_get_coverage_build_dir {host} {
    global ts_config
    global CHECK_COVERAGE_DIR
@@ -52,7 +59,18 @@ proc lcov_get_coverage_build_dir {host} {
    return [file join $CHECK_COVERAGE_DIR $ts_config(commd_port) $host]
 }
 
-# adapt permissions of *.gcda files
+## @brief adapts the permissions of *.gcda files in the coverage build directory
+#
+# *.gcda files are created during execution of the OCS/GCS commands that are build with gcov.
+# The files are executed as the user that triggered the command and the created *.gcda files
+# have permissions 644 (read and write for the owner, read for group and others - with a typical umask).
+#
+# File permissions need to get changed to 666 (read and write for all users) so that the lcov tool can
+# read the coverage data files. and also that OCS/GCS commands can update the coverage data files.
+#
+# @param host the host name for which the permissions are adapted
+# @return 0 on success, -1 on failure
+#
 proc lcov_adapt_permissions {host} {
    set build_dir [lcov_get_coverage_build_dir $host]
    set find_binary [get_binary_path $host "find"]
@@ -65,11 +83,22 @@ proc lcov_adapt_permissions {host} {
    return 0
 }
 
+## @brief returns the lcov command line with all exclude directives
+#
+# The exclude directives are used to exclude directories that should not be part of the coverage report.
+# The directories are:
 # - the _deps directory (cmake dependent projects)
+# - the /home/$CHECK_USER/3rd_party directory (where 3rd party libraries are located)
+# - the build directory (where the build files are located e.g. generated code)
 # - the /opt/rh directory (where C++ compilers are installed and we would see inlined code of C++ headers)
-# - the test directory (where module tests of cluster scheduler are located)
-# - the test directory (where module tests of GCS are located)
-# - the 3rd_party directory (where 3rd party libraries are located)
+# - the /usr/include directory (where C++ headers are located)
+# - the /home/$CHECK_USER/3rd_party/qmake directory (where qmake is located)
+# - the /home/$CHECK_USER/3rd_party/test directory (where module tests of cluster scheduler are located)
+# - the /home/$CHECK_USER/3rd_party/../../$ts_config(ext_source_dir)/test directory (where module tests of GCS are located)
+#
+# @param host the host name for which the lcov command line is returned
+# @return the lcov command line with all exclude directives
+#
 proc lcov_exclude_directives {host} {
    global CHECK_USER
    global ts_config
@@ -86,6 +115,19 @@ proc lcov_exclude_directives {host} {
    return $lcov_base_cmd_line
 }
 
+## @brief initializes the lcov baseline with zero counters
+#
+# This function is called to initialize the lcov baseline with zero counters.
+# It is called during the build process to ensure that zero counters are available for all
+# architectures that are compiled and for all branches of the code.
+# The baseline-zero can later be combined with a test specific coverage snapshot
+# to create a final coverage report that includes all counters. This insures that
+# the final coverage report does not only the counters but also the correct percentages in
+# the reports.
+#
+# @param host the host name for which the lcov baseline is initialized
+# @return 0 on success, -1 on failure
+#
 proc lcov_init_baseline_zero {host} {
    global CHECK_USER
 
@@ -107,9 +149,17 @@ proc lcov_init_baseline_zero {host} {
    set lcov_args "[lcov_exclude_directives $host] -capture -initial -directory $lcov_data_dir -output-file $build_dir/baseline-zero-$arch.info"
    ts_log_fine "lcov_initialize: $lcov_binary $lcov_args"
    set lcov_output [start_remote_prog $host $CHECK_USER $lcov_binary $lcov_args prg_exit_state 300]
+
+   return 0
 }
 
-# callback
+## @brief initializes the lcov coverage data.
+#
+# This function is called to initialize the lcov coverage data or to reset the coverage data.
+# Can be triggered manually via TS menu
+#
+# @param clean if 1 then the coverage data is cleaned before initialization, otherwise the existing data is kept
+# @return 0 on success, -1 on failure
 proc lcov_initialize {{clean 0}} {
    global CHECK_COVERAGE_DIR
    global CHECK_RESULT_DIR
@@ -132,9 +182,17 @@ proc lcov_initialize {{clean 0}} {
          lcov_init_baseline_zero $host
       }
    }
+   return 0
 }
 
-# automatic callback in the testsuite framework triggered directly after the cmake build
+## @brief callback in the testsuite framework triggered directly after the cmake build
+#
+# This function is called to initialize the lcov coverage data after the cmake build.
+# It is called during the build process to ensure that zero counters are available for all
+# architectures that are compiled and for all branches of the code.
+#
+# @return 0 on success, -1 on failure
+#
 proc lcov_build_epilog {} {
    global CHECK_USER
 
@@ -144,7 +202,11 @@ proc lcov_build_epilog {} {
    return 0
 }
 
-# automatic callback in the testsuite framework triggered directly after a test finished
+## @brief callback in the testsuite framework triggered directly after one check of a test finished
+#
+# @param test_name the name of the test that finished
+# @param check_name the name of the check that finished
+# @returns 0 on success, -1 on failure
 proc lcov_check_epilog {test_name check_name} {
    ts_log_fine "lcov_check_epilog: $test_name $check_name"
    foreach host [lcov_get_compile_hosts] {
@@ -154,7 +216,11 @@ proc lcov_check_epilog {test_name check_name} {
    return 0
 }
 
-# automatic callback in the testsuite framework triggered directly after a test finished
+## @brief callback in the testsuite framework triggered directly after one test finished
+#
+# @param test_name the name of the test that finished
+# @returns 0 on success, -1 on failure
+#
 proc lcov_test_epilog {test_name} {
    ts_log_fine "lcov_test_epilog: $test_name"
    foreach host [lcov_get_compile_hosts] {
@@ -165,21 +231,31 @@ proc lcov_test_epilog {test_name} {
    return 0
 }
 
-# callback in the testsuite framework
+# callback in the testsuite framework that would allow per process setup
 proc lcov_per_process_setup {host user env_var} {
    #upvar $env_var env
    # nothing to do for lcov
 }
 
-# callback in the testsuite framework
+# callback in the testsuite framework that would allow to join the coverage data of multiple coverage runs
+# this is not required for lcov as it is done in the lcov_compute_coverage function automatically
 proc lcov_join_dirs {} {
    # @todo CS-1307: all architecture specific coverage is automatically collected in the cmake-build directory
    # this means that there is no need to join data for one architecture ...but
    # is there a possibility to combine the data of multiple architectures?
 }
 
-# callback in the testsuite framework
-# will also be triggered after each test
+## @brief computes the coverage data for the given test name
+#
+# This function is called to compute the coverage data for the given test name.
+# It is called during the test execution to compute the coverage data for the test.
+# In regular intervals the method also updates a final coverage report.
+#
+# Can be triggered manually via TS menu, too, e.g. to generate a final coverage report.
+#
+# @param test_name the name of the test for which the coverage data is computed
+# @note if test_name is "final" then the final coverage report is generated
+#
 proc lcov_compute_coverage {{test_name "final"}} {
    global CHECK_USER
    global ts_config
