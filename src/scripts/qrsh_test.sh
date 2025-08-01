4786372abc630ec408e7fa5e0c6e227112b32066
Joachim Gabler <jgabler@hpc-gridware.com>
2025-08-01 13:58:38 +0200

    fixes to extensive_qrsh test - not all (immediate) qrsh jobs can be started immediately - make sure that they wait (-now no) - wait for accounting records to be flushed to accounting file before checking them

--- src/scripts/qrsh_test.sh
+++ src/scripts/qrsh_test.sh
@@ -6,7 +6,8 @@ qrsh_count=$2
 echo "starting $qrsh_count qrsh calls ..."
 counter=$qrsh_count
 while [ $counter -ne 0 ]; do
-   qrsh -l h=$host hostname &
+   # we can't be sure that the qrsh jobs can be scheduled immediately - submit them as batch jobs
+   qrsh -now no -l h=$host hostname &
    counter=`expr $counter - 1`
 done
 echo "all qrsh calls started"
