350912a89891a40e5048b513cd0633c7831f725d
Joachim Gabler <jgabler@hpc-gridware.com>
2026-03-10 16:05:20 +0100

    EH: CS-955 reduce load on testsuite itself when running the performance/throughput test // made qstat.tcl called in mixed_with_qstat runlevel report data only once a second

--- src/scripts/qstat.tcl
+++ src/scripts/qstat.tcl
@@ -27,7 +27,7 @@
 #
 #  All Rights Reserved.
 #
-#  Portions of this software are Copyright (c) 2023-2024 HPC-Gridware GmbH
+#  Portions of this software are Copyright (c) 2023-2026 HPC-Gridware GmbH
 #
 ##########################################################################
 #___INFO__MARK_END__
@@ -44,8 +44,48 @@ proc get_arch {} {
    }
 }
 
+##
+# @brief send either successfull qstat data or error data
+#
+# @param[in] item the item to send, either "durations" or "error_durations"
+# @param[in] send_name the name to send, either "DATA" or "ERROR"
+proc send_qstat_data_item {item send_name} {
+   global qstat_data
+
+   # only send data if we actually have any
+   set num_data [llength $qstat_data($item)]
+   if {$num_data > 0} {
+      # send data
+      # we send the timetamp and the list of durations
+      puts "QSTAT $send_name $qstat_data(current_time) $qstat_data($item)"
+      set qstat_data($item) {}
+   }
+}
+
+##
+# @brief send qstat data, both successfull and error data
+# @param[in] next_time the next timestamp to send data
+proc send_qstat_data {next_time} {
+   global qstat_data
+
+   send_qstat_data_item "durations" "DATA"
+   send_qstat_data_item "error_durations" "ERROR"
+
+
+   # we store durations until time > next_time
+   set qstat_data(current_time) $next_time
+}
+
+##
+# @brief do a qstat and measure the duration, then send the data if needed
+#
+# The function calls qstat and measures the duration.
+# If the timestamp increased, it sends the data.
+# It returns the number of ms to the next qstat, so that the caller can wait for that time.
+#
+# @return the number of ms to next qstat
 proc do_qstat {} {
-   global active interval qstat_command
+   global active interval qstat_command qstat_data
 
    set start_time [clock clicks -milliseconds]
    if {$active} {
@@ -54,10 +94,17 @@ proc do_qstat {} {
       set end_time [clock clicks -milliseconds]
       set end_clock [clock seconds]
       set duration [expr double($end_time - $start_time) / 1000.0]
+
+      # we send qstat data only once a second
+      # send it if the timestamp increased
+      if {$end_clock > $qstat_data(current_time)} {
+         send_qstat_data $end_clock
+      }
+
       if {$result == 0} {
-         puts "QSTAT DATA $end_clock $duration"
+         lappend qstat_data(durations) $duration
       } else {
-         puts "QSTAT ERROR $end_clock $duration"
+         lappend qstat_data(error_durations) $duration
       }
    }
    set end_time [clock clicks -milliseconds]
@@ -69,7 +116,7 @@ proc do_qstat {} {
       set delay [expr $delay - $milli]
 
       # expect only knows timeouts in seconds.
-      # so wait for the milliseconds, expect will timeout after the seconds 
+      # so wait for the milliseconds, expect will timeout after the seconds
       # part
       after $milli
    } else {
@@ -92,6 +139,9 @@ set arch     [get_arch]
 #set qstat_command "$sge_root/bin/$arch/qstat -f"
 #append qstat_command " -u '*'"
 set qstat_command "$sge_root/bin/$arch/qstat -g c"
+set qstat_data(durations) {}
+set qstat_data(error_durations) {}
+set qstat_data(current_time) 0
 
 set active 0            ;# do measurements
 set interval 1000       ;# default info interval: 1000 milliseconds
@@ -115,10 +165,14 @@ while { !$do_exit } {
       "START\n" {
          set active 1
          puts "QSTAT START OK"
+         # initialize data time stamp
+         set qstat_data(current_time) [clock seconds]
       }
       "STOP\n" {
          set active 0
          puts "QSTAT STOP OK"
+         # there might still be data to be sent
+         send_qstat_data 0
       }
       "*\n" {
          puts "ERROR: invalid input:  $expect_out(0,string)"
